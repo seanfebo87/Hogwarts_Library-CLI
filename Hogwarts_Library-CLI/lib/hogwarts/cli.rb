b04cfc74519e3db962a2eccc873ccb7381b921b6
Sean Febo <sfeebs@Seans-MacBook-Pro.local>
2018-04-25 16:45:34 -0700

    Defined my class variables. These methods also create new objects.

--- Hogwarts_Library-CLI/lib/hogwarts/cli.rb
+++ Hogwarts_Library-CLI/lib/hogwarts/cli.rb
@@ -9,7 +9,15 @@ def start
     @@atlas = create_atlas_array
 end
 
-
-
+def create_books_array
+    textbooks_array = Hogwarts::Textbooks.scrape_textbooks
+    textbooks_array.map do |book|
+      Hogwarts::Textbooks.new(book[:name], book[:url])
+def create_atlas_array
+    atlas_array = Hogwarts::Atlas.scrape_atlas
+    atlas_array.map do |n|
+      Hogwarts::Atlas.new(n[:name], n[:url])
+    end
+end
 
 end
