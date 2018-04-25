d856cbeb2b27bff3e6cd6b38be0494085175fc75
Sean Febo <sfeebs@Seans-MacBook-Pro.local>
2018-04-25 16:36:46 -0700

    Added scraping method for Hogwarts::Atlas

--- Hogwarts_Library-CLI/lib/hogwarts/atlas.rb
+++ Hogwarts_Library-CLI/lib/hogwarts/atlas.rb
@@ -12,3 +12,17 @@ class Hogwarts::Atlas
      @url = url
      @@all << self
     end
+
+    def self.scrape_atlas
+        maps = []
+        doc = Nokogiri::HTML(open("https://www.hp-lexicon.org/list/wizarding-atlas/atlas-of-hogwarts/"))
+        info = doc.css(".col-md-12 ul li a")
+        info.each do |n|
+            atlas = {}
+            atlas[:name] = n.text
+            atlas[:url] = n.attr("href")
+            maps << atlas
+
+        end
+        maps
+    end
