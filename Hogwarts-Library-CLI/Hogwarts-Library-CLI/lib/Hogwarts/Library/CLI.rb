ee259d9671880ef881e7c476d01226659cd07c0d
Sean Febo <sfeebs@Seans-MacBook-Pro.local>
2018-04-25 20:55:07 -0700

    Gem files were not working. Created new gem.

---
+++ Hogwarts-Library-CLI/Hogwarts-Library-CLI/lib/Hogwarts/Library/CLI.rb
@@ -0,0 +1,9 @@
+require "Hogwarts/Library/CLI/version"
+
+module Hogwarts
+  module Library
+    module CLI
+      # Your code goes here...
+    end
+  end
+end
---
+++ Hogwarts-Library-CLI/Hogwarts-Library-CLI/lib/Hogwarts/Library/CLI/version.rb
@@ -0,0 +1,7 @@
+module Hogwarts
+  module Library
+    module CLI
+      VERSION = "0.1.0"
+    end
+  end
+end
